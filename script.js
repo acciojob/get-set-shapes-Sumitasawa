7d4f8386b7813cb914a33469242553f0974aa782
Sumitasawa <132041180+Sumitasawa@users.noreply.github.com>
2025-08-07 13:43:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-//complete this code
-class Rectangle{
-	constructor(width,height){
-	this.width=width;
-	this.height=height;
-	}
-	get width(){
-		return  this._width;
-	}
-	get height(){
-		return this._height;
-	}
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-	get area(){
-		return this._width*this._height;
-	}
+  get width() {
+    return this._width;
+  }
+
+  get height() {
+    return this._height;
+  }
+
+  getArea() {
+    return this._width * this._height;
+  }
 }
 
-class Square extends Animal {
-	constructor(side){
-		super(side,side);
-		this.side=side;
-	}
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side); // Call Rectangle constructor
+  }
 
-	get Perimeter(){
-		return 4*this._side;
-	}
+  getPerimeter() {
+    return 4 * this.width;
+  }
 }
 
 // Do not change the code below this line
